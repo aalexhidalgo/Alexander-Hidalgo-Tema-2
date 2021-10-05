1c309fc1c3c6e8ecc53048232b4fd7c9cc4f4b36
aalexhidalgo <alexhidalgocalafat@gmail.com>
2021-10-05 12:57:30 +0200

    Tema 2

---
+++ Tema 2/Assets/Scripts/MyFirstScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyFirstScript : MonoBehaviour
+{
+    public int playerAge = 17;
+    private float playerSpeed = 5.25f;
+    public string playerName = "Alexander";
+    [SerializeField] private bool game0ver= false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
