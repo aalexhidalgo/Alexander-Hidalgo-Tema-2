17aac46bc886805acb46f10eef275a52b68cf2d0
aalexhidalgo <alexhidalgocalafat@gmail.com>
2021-11-08 10:39:54 +0100

    Tema 2

--- Tema 2/Assets/Scripts/HotPotato.cs
+++ Tema 2/Assets/Scripts/HotPotato.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class HotPotato : MonoBehaviour
 {
     private int Counter;
+    public Color colorMat;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,7 @@ public class HotPotato : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        GetComponent<MeshRenderer>().material.color = colorMat;
     }
 
     private void OnMouseDown()
